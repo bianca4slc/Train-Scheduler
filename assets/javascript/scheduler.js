633d012674128f56535a545aa1fdd025dedc44f1
3 Bianc$ <56326348+bianca4slc@users.noreply.github.com>
2020-03-18 18:48:01 -0600

    working on moment

--- assets/javascript/scheduler.js
+++ assets/javascript/scheduler.js
@@ -1,7 +1,15 @@
 // initialize firebase
-// var config = {};
+var firebaseConfig = {
+  apiKey: "AIzaSyDQA_27i_CrkxtM6I3yDu85klhn8NbCB4g",
+  authDomain: "train-scheduler-cbo.firebaseapp.com",
+  databaseURL: "https://train-scheduler-cbo.firebaseio.com",
+  projectId: "train-scheduler-cbo",
+  storageBucket: "train-scheduler-cbo.appspot.com",
+  messagingSenderId: "189207594546",
+  appId: "1:189207594546:web:c1ac931a8a0d0df4fcde2b"
+};
 
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
@@ -16,12 +24,10 @@ $("#add-train-button").on("click", function(event) {
   var trainDest = $("#destination-input")
     .val()
     .trim();
-  var trainTime = moment(
-    $("#first-train-input")
-      .val()
-      .trim(),
-    "HH:mm"
-  ).format("x");
+  var trainTime = $("#first-train-input")
+    .val()
+    .trim();
+
   var trainFreq = $("#frequency-input")
     .val()
     .trim();
@@ -50,40 +56,68 @@ $("#add-train-button").on("click", function(event) {
   $("#frequency-input").val("");
 });
 
-// create firebase event for adding employee to the database and row in the html
+// create firebase event for adding train to the database and row in the html
+database.ref().on("child_added", function(childSnapshot) {
+  // store everything into a variable
+  var trainName = childSnapshot.val().name;
+  var trainDest = childSnapshot.val().destination;
+  var trainTime = childSnapshot.val().time;
+  var trainFreq = parseInt(childSnapshot.val().frequency);
 
-// store everything into a variable
-var trainName = childSnapshot.val().name;
-var trainDest = childSnapshot.val().destination;
-var trainTime = childSnapshot.val().time;
-var trainFreq = childSnapshot.val().frequency;
+  // train info
+  console.log(trainName);
+  console.log(trainDest);
+  console.log(trainTime);
+  console.log(typeof trainFreq);
 
-// train info
-console.log(trainName);
-console.log(trainDest);
-console.log(trainTime);
-console.log(trainFreq);
+  // pretty train start
+  var trainStartPretty = moment.unix(trainTime).format("HH:mm");
 
-// pretty train start
-var trainStartPretty = moment.unix(trainTime).format("HH:mm");
+  // calculate next arrival
+  // var nextTrain = moment().diff
+  // console.log(nextTrain)
 
-// calculate next arrival
-// var nextTrain = moment().diff
-// console.log(nextTrain)
+  // calculate minutes away
+  // var minAway =
+  // Assumptions
+  var tFrequency = trainFreq;
 
-// calculate minutes away
-// var minAway =
+  // Time is 3:30 AM
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
+  console.log(firstTimeConverted);
 
-// create new row
-var newRow = $("<tr>").append(
-  $("<td>").text(trainName),
-  $("<td>").text(trainDest),
-  $("<td>").text(trainTime),
-  $("<td>").text(trainFreq),
-  $("<td>").text(nextTrain),
-  $("<td>").text(minAway)
-);
+  // Current Time
+  var currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-// append new row to table
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  // console.log("DIFFERENCE IN TIME: " + diffTime);
+  console.log(diffTime);
 
-$("#train-table > tbody").append(newRow);
+  // Time apart (remainder)
+  var tRemainder = diffTime % trainFreq;
+  console.log(tRemainder);
+
+  // Minute Until Train
+  var minAway = trainFreq - tRemainder;
+  console.log("MINUTES TILL TRAIN: " + minAway);
+
+  // Next Train
+  var newTrain = moment().add(minAway, "minutes");
+  console.log("ARRIVAL TIME: " + moment(newTrain).format("hh:mm"));
+
+  // create new row
+  var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(trainDest),
+    $("<td>").text(trainTime.moment.unix(trainTime).format("HH:mm")),
+    $("<td>").text(trainFreq),
+    $("<td>").text(newTrain),
+    $("<td>").text(minAway)
+  );
+
+  // append new row to table
+  $("#train-table > tbody").append(newRow);
+});
